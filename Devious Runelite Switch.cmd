eff3129d74d46e11c69f34f993a80f7367409220
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-12 02:33:28 +1000

    Update Devious Runelite Switch.cmd

--- Devious Runelite Switch.cmd
+++ Devious Runelite Switch.cmd
@@ -1,18 +1,18 @@
-
 set runelite_folder=%LOCALAPPDATA%\Runelite
 
 if exist "%runelite_folder%\devious-client-launcher.jar" (
     ren "%runelite_folder%\runelite.jar" "runelite-default.jar"
     ren "%runelite_folder%\devious-client-launcher.jar" "runelite.jar"
-    echo You are now running Devious.
-    pause
+    cls
+    echo Switched to Devious client successfully.
 ) else if exist "%runelite_folder%\runelite-default.jar" (
     ren "%runelite_folder%\runelite.jar" "devious-client-launcher.jar"
     ren "%runelite_folder%\runelite-default.jar" "runelite.jar"
-    echo You are now running default Runelite.
-    pause
+    cls
+    echo Switched to Default Runelite successfully.
 ) else (
     echo Unable to detect Runelite version.
     pause
 )
+
 pause
