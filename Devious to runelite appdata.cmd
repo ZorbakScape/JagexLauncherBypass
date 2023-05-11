c150d9cfc384ab0c7ce292ddac97518ae61cc7f9
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-12 01:11:43 +1000

    Update Devious to runelite appdata.cmd

--- Devious to runelite appdata.cmd
+++ Devious to runelite appdata.cmd
@@ -1,5 +1,14 @@
-IF EXIST %userprofile%\Downloads\devious-client-launcher.jar (
-    copy %userprofile%\Downloads\devious-client-launcher.jar %userprofile%\AppData\local\runelite\devious-client-launcher.jar
- ) ELSE ( 
-    curl.exe --output %userprofile%\AppData\local\runelite\devious-client-launcher.jar --url https://github.com/jbx5/devious-client/releases/download/launcher-1.0.1/devious-client-launcher.jar
- )
+set devious_launcher=https://github.com/jbx5/devious-client/releases/download/launcher-1.0.1/devious-client-launcher.jar
+set download_folder=%USERPROFILE%\Downloads
+set runelite_folder=%LOCALAPPDATA%\runelite
+
+if exist "%download_folder%\devious-client-launcher.jar" (
+    echo Devious launcher already exists in Downloads. Copying to Runelite folder...
+    copy "%download_folder%\devious-client-launcher.jar" "%runelite_folder%\devious-client-launcher.jar"
+) else (
+    echo Devious launcher not found in Downloads. Downloading from GitHub...
+    curl.exe --output "%runelite_folder%\devious-client-launcher.jar" --url "%devious_launcher%"
+)
+
+echo Devious launcher installation complete.
+pause
