1d8a54bb8a6e52b9304f8556d6639beab3e16296
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-11 18:23:32 +1000

    Update and rename devious to runelite.cmd to Devious Runelite Switch.cmd

---
+++ Devious Runelite Switch.cmd
@@ -0,0 +1,7 @@
+IF EXIST %userprofile%\Appdata\local\runelite\devious-client-launcher.jar (
+    ren %userprofile%\Appdata\local\runelite\runelite.jar %userprofile%\Appdata\local\runelite\runelite-default.jar 
+	ren %userprofile%\Appdata\local\runelite\devious-client-launcher.jar %userprofile%\Appdata\local\runelite\runelite.jar 
+ ) ELSE ( 
+    ren %userprofile%\Appdata\local\runelite\runelite.jar %userprofile%\Appdata\local\runelite\rdevious-client-launcher.jar
+	ren %userprofile%\Appdata\local\runelite\runelite-default.jar %userprofile%\Appdata\local\runelite\runelite.jar
+ )
--- devious to runelite.cmd
+++
@@ -1,7 +0,0 @@
-cd %appdata%
-cd ..
-cd local
-cd "Runelite"
-ren runelite.jar devious-client-launcher.jar
-ren runelite-default.jar runelite.jar
-pause
