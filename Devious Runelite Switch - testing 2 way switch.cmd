7608e450a8649fdad950704308d645816a9952d1
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-11 22:08:04 +1000

    Update Devious Runelite Switch - testing 2 way switch.cmd

--- Devious Runelite Switch - testing 2 way switch.cmd
+++ Devious Runelite Switch - testing 2 way switch.cmd
@@ -1,10 +1,10 @@
- IF EXIST %userprofile%\AppData\Local\Runelite\devious-client-launcher.jar (
+@echo off
+IF EXIST %userprofile%\AppData\Local\Runelite\devious-client-launcher.jar (
     cd %userprofile%\AppData\Local\Runelite\
     ren runelite.jar runelite-default.jar
-	ren devious-client-launcher.jar runelite.jar
- ) ELSE ( 
+    ren devious-client-launcher.jar runelite.jar
+) ELSE ( 
     cd %userprofile%\AppData\Local\Runelite\
-	ren runelite.jar devious-client-launcher.jar
-	ren runelite-default.jar runelite.jar
- )
- pause
+    ren runelite.jar devious-client-launcher.jar
+    ren runelite-default.jar runelite.jar
+)
