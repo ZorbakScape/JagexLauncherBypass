5110c7bf0b1de172c640aff2acb456ac37cbd529
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-12 01:43:16 +1000

    Update meteor runelite switcher.cmd

--- meteor runelite switcher.cmd
+++ meteor runelite switcher.cmd
@@ -1,1 +1,48 @@
+@echo off
+
+set runelite_folder=%LOCALAPPDATA%\runelite
+set meteor_folder=%LOCALAPPDATA%\meteor
+set revert_flag_file=revert_flag
+
+if exist "%revert_flag_file%" (
+    echo Reverting changes...
+    if exist "%meteor_folder%\runelite.exe" (
+        ren "%meteor_folder%\runelite.exe" "meteor-launcher.exe"
+        ren "%meteor_folder%\app\runelite.cfg" "meteor-launcher.cfg"
+    )
+    if exist "%runelite_folder%\meteor-launcher.cfg" (
+        ren "%runelite_folder%\meteor-launcher.exe" "runelite.exe"
+        ren "%runelite_folder%\meteor-launcher.cfg" "runelite.cfg"
+    )
+    ren "%meteor_folder%" "Runelite"
+    ren "%runelite_folder%" "runelite-default"
+    del "%revert_flag_file%"
+    goto :EOF
+)
+
+echo Checking if Meteor is installed...
+
+if exist "%meteor_folder%\runelite.exe" (
+    echo Meteor is currently installed.
+    set is_meteor=true
+) else if exist "%runelite_folder%\runelite.exe" (
+    echo Default Runelite is currently installed.
+    set is_meteor=false
+) else (
+    echo Unable to detect default installation.
+    pause
+    goto :EOF
+)
+
+ren "%runelite_folder%" "runelite-default"
+
+ren "%meteor_folder%" "Runelite"
+
+ren "%meteor_folder%\runelite.exe" "runelite.exe"
+
+if %is_meteor%==true (
+    ren "%meteor_folder%\app\runelite.cfg" "runelite.cfg"
+) else (
+    echo Configuration file is not available in Meteor folder. Skipping.
+)
 
