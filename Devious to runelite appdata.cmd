8f00f48e91b27b9ced759edd0af89e75fbdb5ae0
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-05-12 01:51:00 +1000

    Update Devious to runelite appdata.cmd

--- Devious to runelite appdata.cmd
+++ Devious to runelite appdata.cmd
@@ -1,14 +1,18 @@
+@echo off
+
 set devious_launcher=https://github.com/jbx5/devious-client/releases/download/launcher-1.0.1/devious-client-launcher.jar
 set download_folder=%USERPROFILE%\Downloads
-set runelite_folder=%LOCALAPPDATA%\runelite
 
 if exist "%download_folder%\devious-client-launcher.jar" (
-    echo Devious launcher already exists in Downloads. Copying to Runelite folder...
-    copy "%download_folder%\devious-client-launcher.jar" "%runelite_folder%\devious-client-launcher.jar"
+    set /p delete_launcher=Devious launcher file found in Downloads folder. Would you like to delete it? (Y/N)
+    if /i {%delete_launcher%} == {Y} (
+        echo Deleting downloaded Devious launcher file...
+        del "%download_folder%\devious-client-launcher.jar"
+    ) else (
+        echo Not deleting downloaded Devious launcher file.
+    )
 ) else (
-    echo Devious launcher not found in Downloads. Downloading from GitHub...
-    curl.exe --output "%runelite_folder%\devious-client-launcher.jar" --url "%devious_launcher%"
+    echo Devious launcher not found in Downloads folder.
 )
 
-echo Devious launcher installation complete.
 pause
