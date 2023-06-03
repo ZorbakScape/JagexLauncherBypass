6109ced9e0c094ad5736a6759d55105e59734ab5
ZorbakScape <132999621+ZorbakScape@users.noreply.github.com>
2023-06-03 20:32:17 +1000

    Update Devious to runelite appdata.cmd

--- Devious to runelite appdata.cmd
+++ Devious to runelite appdata.cmd
@@ -1,9 +1,3 @@
 @echo off
 
-set devious_launcher=https://github.com/jbx5/devious-client/releases/download/launcher-1.0.1/devious-client-launcher.jar
-set runelite_folder=%LOCALAPPDATA%\Runelite
-
-if exist "%runelite_folder%\devious-client-launcher.jar" (
-) else (
-    curl.exe --location --output "%runelite_folder%\devious-client-launcher.jar" %devious_launcher%
-    
+powershell.exe -ExecutionPolicy Bypass -Command "$sourceUrl = 'https://github.com/jbx5/devious-client/releases/download/launcher-1.0.1/devious-client-launcher.jar'; $output = $env:LOCALAPPDATA + '\Runelite\devious-client-launcher.jar'; if (-not (Test-Path $output)) { Invoke-WebRequest -Uri $sourceUrl -OutFile $output }"
